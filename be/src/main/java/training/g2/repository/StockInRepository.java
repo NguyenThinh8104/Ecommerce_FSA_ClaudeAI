886be79574ebd64cf7275cd5697d8aa3582b9e77
thinhnphe181767 <nguyenphuthinh08012004@gmail.com>
2025-11-17 18:01:53 +0700

    Remerge

--- be/src/main/java/training/g2/repository/StockInRepository.java
+++
@@ -1,9 +0,0 @@
-package training.g2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import training.g2.model.StockIn;
-
-@Repository
-public interface StockInRepository extends JpaRepository<StockIn, Long> {
-}
